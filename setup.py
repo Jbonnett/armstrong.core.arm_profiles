d81ab2f1b906fd5755983954985122d5373aad02
Peter Gullekson <pgullekson@baycitizen.org>
2011-04-12 11:58:33 -0700

    added license and setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='armstrong.core.arm_profiles',
+    version='0.1',
+    description='Provides the user profiles',
+    author='Bay Citizen & Texas Tribune',
+    author_email='info@armstrongcms.org',
+    url='http://github.com/armstrongcms/armstrong.core.arm_profiles/',
+    packages=[
+        'armstrong',
+        'armstrong.core',
+        'armstrong.core.arm_profiles',
+    ],
+
+    install_requires=[
+        'distribute',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
