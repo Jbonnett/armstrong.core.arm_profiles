cd049e3739f4cdea11fe6ae11968b0b6cfc0b586
Peter Gullekson <pgullekson@baycitizen.org>
2011-04-18 12:22:20 -0700

    added UserProfile model

--- armstrong/core/arm_profiles/models.py
+++ armstrong/core/arm_profiles/models.py
@@ -1,27 +1,23 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.core.urlresolvers import reverse
-
 from sorl.thumbnail import ImageField
 
+
 class UserProfile(models.Model):
+
     user = models.ForeignKey(User, unique=True)
     active = models.BooleanField(default=True)
-    slug = models.SlugField()
-
-    about = models.TextField(null=True, blank=True)
-    photo = ImageField()
-    website = models.URLField(null=True, blank=True, verify_exists=False)
-
-    facebook_name = models.CharField(max_length=100, blank=True)
-    twitter_name = models.CharField(max_length=100, blank=True)
-    linkedin_url = models.URLField(max_length=100, verify_exists=False,
-        blank=True, null=True)
-
-    show_email = models.BooleanField(default=False)
+    public = models.BooleanField(default=True)
+    location = models.CharField(max_length=40, blank=True)
+    about = models.TextField(blank=True)
+    website = models.URLField(blank=True, verify_exists=False)
+    photo = ImageField(upload_to=getattr(settings,
+            'ARM_PROFILES_PHOTO_PATH', 'arm_profiles/photos'))
 
     def __unicode__(self):
         return '%s %s' % (self.user.first_name, self.user.last_name)
 
     def get_absolute_url(self):
-        return reverse('profiles_detail', kwargs={'profile_slug': self.slug})
+        return reverse('profile_detail', kwargs={'username': self.user.username})
