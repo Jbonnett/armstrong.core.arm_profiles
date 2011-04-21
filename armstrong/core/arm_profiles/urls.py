406ea5fa69b367c2c198664a5eb13c8505bee1e8
Peter Gullekson <pgullekson@baycitizen.org>
2011-04-21 16:52:52 -0700

    removed unnecessary imports

--- armstrong/core/arm_profiles/urls.py
+++ armstrong/core/arm_profiles/urls.py
@@ -1,7 +1,4 @@
 from django.conf.urls.defaults import *
-from django.views.generic.create_update import update_object
-from django.shortcuts import get_object_or_404
-from armstrong.core.arm_profiles.models import UserProfile
 from armstrong.core.arm_profiles.views import (UserProfileDetailView,
         UserProfileUpdateView)
 
