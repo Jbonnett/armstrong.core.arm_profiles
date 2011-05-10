f373d7893d719747a63e2f766670e991ebcc53e9
Peter Gullekson <peter11235@gmail.com>
2011-05-10 15:51:48 -0700

    cleaned tests

--- armstrong/core/arm_profiles/tests/models.py
+++ armstrong/core/arm_profiles/tests/models.py
@@ -16,9 +16,6 @@ class UserProfileTestCase(TestCase):
         profile.about = "american soooul singer"
         profile.save()
 
-        self.user2 = User.objects.create_user('paolo',
-            'paolo@armstrongcms.org', 'pwrd')
-
         self.c = Client()
         self.assertEqual(self.c.login(username='otis', password='pw'), True)
 
@@ -31,27 +28,18 @@ class UserProfileTestCase(TestCase):
         self.assertContains(response, self.user.first_name)
         self.assertContains(response, self.user.last_name)
 
-    def test_get_edit_form(self):
-        response = self.c.get(reverse('profile_edit'))
-        self.assertTrue('profile_form' in response.context)
-
-    def test_deny_edit_form(self):
-        self.c.logout()
-        response = self.c.get(reverse('profile_edit'))
-        self.assertRedirects(response, '%s?next=%s' % (getattr(settings, 'LOGIN_URL'), reverse('profile_edit')))
-
     def test_no_profile_found(self):
         response = self.c.get(reverse('profile_detail', kwargs={'username':'crocker'}))
         self.assertEqual(response.status_code, 404)
 
     def test_non_public_profile(self):
-        self.user2.get_profile().public = False
-        self.user2.get_profile().save()
-        response = self.c.get(self.user2.get_absolute_url())
-        self.assertNotContains(response, self.user2.email)
+        self.user.get_profile().public = False
+        self.user.get_profile().save()
+        response = self.c.get(self.user.get_absolute_url())
+        self.assertNotContains(response, self.user.email)
 
     def test_non_active_profile(self):
-        self.user2.is_active = False
-        self.user2.save()
-        response = self.c.get(self.user2.get_absolute_url())
+        self.user.is_active = False
+        self.user.save()
+        response = self.c.get(self.user.get_absolute_url())
         self.assertEqual(response.status_code, 404)
